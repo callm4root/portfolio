38c822b8afd6383d73711c3f51d514f44992f0ca
harold699 <haroldnextel361@gmail.com>
2025-07-17 13:14:24 +0100

    qss

---
+++ a.js
@@ -0,0 +1,7 @@
+AOS.init({
+  duration: 100,
+  once: false,    // false = animation rejouée à chaque fois que l'élément entre dans le viewport
+  mirror: true    // permet aussi de rejouer l'animation quand on remonte (scroll up)
+});
+
+
